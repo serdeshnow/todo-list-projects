194cb072231fd7e2ca7d501de09fcd876032694b
serdeshnow <vserdes1977@mail.ru>
2024-08-30 00:27:34 +0300

    upd: initial commit

---
+++ app-json-placeholder/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./assets/svg/logo.svg";
+import "./styles/App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code>.
+				</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
