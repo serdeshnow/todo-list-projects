eacb8b39ff902d4b7f1ff87a07bf0148490a4ea4
serdeshnow <vserdes1977@mail.ru>
2024-08-30 15:04:19 +0300

    upd: upload changes

--- app-json-server/src/components/Loader/Loader.jsx
+++ app-json-server/src/components/Loader/Loader.jsx
@@ -2,9 +2,9 @@ import "./Loader.scss";
 
 export const Loader = () => {
 	return (
-		<li className="loader__wrapper flex--column">
+		<div className="loader__wrapper flex--column">
 			<p className="loader__description">Loading...</p>
 			<div className="loader" />
-		</li>
+		</div>
 	);
 };
--- app-json-server/src/components/index.js
+++ app-json-server/src/components/index.js
@@ -1,3 +1,4 @@
 export * from "./Header/Header.jsx";
 export * from "./Form/Form.jsx";
 export * from "./Loader/Loader.jsx";
+export * from "./Todos/Todos.jsx";
